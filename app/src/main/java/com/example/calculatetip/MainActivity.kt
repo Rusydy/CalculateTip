ceef0996cf1c1d4acb0e530944e9712028525d5c
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-09-01 05:50:19 +0700

    Add functionality for bill amount

--- app/src/main/java/com/example/calculatetip/MainActivity.kt
+++ app/src/main/java/com/example/calculatetip/MainActivity.kt
@@ -14,14 +14,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculatetip.ui.theme.CalculateTipTheme
@@ -40,6 +45,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipLayout() {
+    val billAmount = remember { mutableStateOf("") }
+    val tipAmount = remember { mutableDoubleStateOf(0.00) }
+
     Box(
         modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
@@ -59,15 +67,20 @@ fun TipLayout() {
             )
 
             TextField(
-                value = "",
-                onValueChange = {},
+                value = billAmount.value,
+                onValueChange = {
+                    billAmount.value = it
+                    tipAmount.doubleValue = calculateTip(billAmount.value.toDouble())
+                },
                 label = { Text(text = stringResource(R.string.bill_amount)) },
-                modifier = Modifier.fillMaxWidth()
-                    .padding(bottom = 16.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
             )
 
             Text(
-                text = stringResource(R.string.tip_amount, "$0.00"),
+                text = stringResource(R.string.tip_amount, String.format("%.2f", tipAmount.doubleValue)),
                 fontWeight = FontWeight.Bold,
                 style = MaterialTheme.typography.displaySmall
             )
@@ -77,6 +90,10 @@ fun TipLayout() {
     }
 }
 
+// calculate tip by multiplying bill amount by 0.15
+fun calculateTip(billAmount: Double): Double {
+    return billAmount * 0.15
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
